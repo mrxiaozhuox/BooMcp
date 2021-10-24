42358d709864810d664606a533a42d5f9767f8d4
mrxiaozhuox <mrxzx@wwsg18.top>
2021-10-24 17:42:35 +0800

    feat: commit code

--- service/main.go
+++ service/main.go
@@ -8,6 +8,9 @@ package service
 import (
 	"net/http"
 
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,13 +18,38 @@ func InitServer() {
 
 	service := gin.Default()
 
-	// 尝试加载 template 目录下的所有页面模板文件
-	service.LoadHTMLGlob("template/*")
+	store := cookie.NewStore([]byte("secret"))
+	service.Use(sessions.Sessions("fkyos", store))
 
-	service.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title": "Main website",
-		})
+	// 尝试加载 template 目录下的所有页面模板文件
+	service.LoadHTMLGlob("template/**/*")
+
+	service.GET("/center/", func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "/center/dashboard")
+	})
+
+	service.GET("/center/:page", func(c *gin.Context) {
+
+		page := c.Param("page")
+
+		session := sessions.Default(c)
+
+		// 查询不到用户的登陆信息，跳转到登陆页面
+		if session.Get("UserID") == nil {
+			c.Redirect(http.StatusMovedPermanently, "/login")
+			return
+		}
+
+		if page == "dashboard" {
+			// DashBoard 页面操作
+			c.HTML(http.StatusOK, "center/dashboard.tmpl", gin.H{
+				"title":              "FkyOS Server",
+				"AcDashboard":        true,
+				"Username":           "mrxiaozhuox",
+				"OnlineServerNumber": 0,
+			})
+			return
+		}
 	})
 
 	service.Run()

c943871585de620bce1223c66f12e47ff550a994
mrxiaozhuox <mrxzx@wwsg18.top>
2021-10-24 21:23:15 +0800

    feat: commit code

--- library/config.go
+++ library/config.go
@@ -1,19 +1,52 @@
 package library
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 )
 
+type generalConfig struct {
+	Hostname string
+	Port     int8
+	TLS      gctls
+}
+
+type gctls struct {
+	PemFile string
+	KeyFile string
+}
+
 func InitConfig() bool {
+
 	ex, err := os.Executable()
 	if err != nil {
 		return false
 	}
+
 	exPath := filepath.Dir(ex)
 
-	fmt.Println(exPath)
-
 	return true
+}
+
+func exists(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil {
+		if os.IsExist(err) {
+			return true
+		}
+		return false
+	}
+	return true
+}
+
+func isDir(path string) bool {
+	s, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return s.IsDir()
+}
+
+func isFile(path string) bool {
+	return !isDir(path)
 }

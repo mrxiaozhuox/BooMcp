6bc1e6bd6413b03dd0d1ee4d33748ba39a103926
mrxiaozhuox <mrxzx@wwsg18.top>
2021-10-28 08:36:25 +0800

    feat: commit code

--- library/mail.go
+++ library/mail.go
@@ -1,5 +1,34 @@
 package library
 
-func SendEmail(conf EmailConfig) {
+import (
+	"os"
+	"path"
+	"path/filepath"
+
+	"gopkg.in/gomail.v2"
+)
+
+func SendEmail(conf EmailConfig, tmpl string) (bool, error) {
+
+	dia := gomail.NewDialer(conf.Server, conf.Port, conf.Username, conf.Password)
+
+	ex, err := os.Executable()
+	if err != nil {
+		return false, err
+	}
+	rootPath := filepath.Dir(ex)
+	templatePath := path.Join(rootPath, "template", "email", tmpl+".tmpl")
+
+	f, err := os.Open(templatePath)
+	if err != nil {
+		return false, err
+	}
+
+	var buffer []byte
+
+	_, err = f.Read(buffer)
+	if err != nil {
+		return false, err
+	}
 
 }
